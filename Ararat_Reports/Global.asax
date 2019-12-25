85b373197c989c1733b220be86b1460288c1f8d4
ابو رضا <hko-1992@hotmail.com>
2019-12-25 23:18:10 +0200

    انشاء المشروع 2

---
+++ Ararat_Reports/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ararat_Reports.MvcApplication" Language="C#" %>
---
+++ Ararat_Reports/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ararat_Reports.Startup))]
+namespace Ararat_Reports
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
